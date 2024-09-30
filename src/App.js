cafe24915e7e52d5bbc7c7add83c7b5915316d1c
yahdielo <yahdielaz@gmail.com>
2024-09-30 08:40:17 -0400

    mango initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import MangoLogo from './imgs/Mango.png';
+
+import { Navbar, Container, Button,Card, Form, Row, Col}from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+    {/* Header*/}
+      <Navbar bg="light" expand="lg" 
+        >
+        <Container> 
+          <img
+            src={MangoLogo}
+            width="50"
+            height="50"
+          />
+          <div className="ml-auto">
+            <Button variant="primary">Connect Wallet</Button>
+          </div>
+        </Container>
+      </Navbar>
+       {/* Centered Box */}
+       <Container className="d-flex justify-content-center align-items-center" style={{ height: '80vh' }}>
+        <Card style={{ width: '24rem' }} className="p-4 shadow">
+          <Card.Body>
+    
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="0.0" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>To</Form.Label>
+                <Form.Control type="text" placeholder="0.0" />
+              </Form.Group>
+              <Button variant="primary" className="w-100">Swap</Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
