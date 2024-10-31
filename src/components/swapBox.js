e59ca9480ea677e4de141c0aed1d903610554557
yahdielo <yahdielaz@gmail.com>
2024-10-30 21:56:31 -0400

    currently disply showingon box2

--- src/components/swapBox.js
+++ src/components/swapBox.js
@@ -73,8 +73,8 @@ const SwapBox = () => {
                 const buyTokenAddress = selectedToken2.address;
                 const amountToSell = amount1*10**selectedToken1.decimals;
                 const resp = await fetchAmountOut(sellTokenAddress,buyTokenAddress,amountToSell);
-                const amountBack = resp.data.buyAmount /(10**selectedToken2.decimals)
-                console.log('in handle blur:',amountBack)
+                console.log('resp',resp)
+                const amountBack = resp.buyAmount /(10**selectedToken2.decimals)
             
                 setOutputAmount(amountBack);
             }
