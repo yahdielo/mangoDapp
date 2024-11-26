7d26f193e2a1c57319501007a762ff738a4125de
yahdielo <yahdielaz@gmail.com>
2024-11-26 19:05:08 -0400

    separated the select token button logic from the swap box component

--- src/components/selecTokenButton.js
+++ src/components/selecTokenButton.js
@@ -1,17 +1,42 @@
-import { Button } from 'react-bootstrap';
 
-const SelectTokenButton = () => {
-
-    return(
+import { Button, Image } from 'react-bootstrap';
+const SelectTokenButton = ({ isSelected, token, onClick, style }) => {
+    return (
         <Button
             variant="outline-secondary"
-            style={{ position: 'absolute', right: '30px', top: '50%', transform: 'translateY(-50%)', padding: '0 5px', 
-            width: "85px",  // Fixed width
-            height: "40px",  // Fixed height
-            textAlign: "center", // Center text
+            onClick={onClick}
+            style={{
+                position: 'absolute',
+                right: '30px',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                padding: '0 5px',
+                width: '95px',
+                height: '40px',
+                textAlign: 'center',
+                fontSize: '0.8rem',
+                backgroundColor: isSelected ? undefined : "#F26E01", // Mango orange for unselected
+                borderColor: isSelected ? undefined : '#FFA500', // Match the border color
+                color: isSelected ? undefined : '#FFFFFF', // White text for unselected
+              
             }}
-        >Select Token</Button>
+        >
+            {isSelected ? (
+                <>
+                    <Image
+                        src={token.image}
+                        alt={token.symbol}
+                        roundedCircle
+                        style={{ width: '24px', height: '24px', marginRight: '5px' }}
+                    />
+                    <span style={{ fontSize: '0.8rem' }}>{token.symbol}</span>
+                </>
+            ) : (
+                'Select Token'
+            )}
+        </Button>
     );
+};
 
-}
 export default SelectTokenButton;
+
