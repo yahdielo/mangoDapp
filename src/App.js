d0712d7ce9efc7d0e5cc3a71e2ac2a4f9b02bc8d
yahdielo <yahdielaz@gmail.com>
2024-11-30 14:14:38 -0400

    change the thirwebWrapper to the index file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Importing react-query provider
-import { 
-      ThirdwebProvider,
-      ChainId,
-      metamaskWallet,
-      walletConnect
-    } 
-    from "@thirdweb-dev/react";
+
+import Client from './client.js';
 import { Container,Navbar} from 'react-bootstrap';
 import SwapBox from "./components/swapBox.js";
 import Header from "./components/header.js"
@@ -15,19 +9,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
-// Create a QueryClient instance  
-const queryClient = new QueryClient();
 const activeChain = "base";
 //const metamaskConfig = metamaskWallet();
 function App() {
-  
   return (
     //wrapping the app on the @tanstack/react-query
     //also wrapping the react components in the thirdweb cli
-    <QueryClientProvider client={queryClient}>
-    <ThirdwebProvider activeChain={activeChain} supportedWallets={[walletConnect(),metamaskWallet()]}>
-      
     <div className="App"> 
     {/* Header*/}
       <Header/>
@@ -36,11 +23,10 @@ function App() {
         style={{
           background: 'linear-gradient(150deg,orange, yellow, green)',  // Gradient background
         }}> 
+      
         <SwapBox/>
       </div>
     </div>
-    </ThirdwebProvider>
-    </QueryClientProvider>
   );
 }
 
