23a3fd662901c780ae27ca67e0e33aa35439bc52
yahdielo <yahdielaz@gmail.com>
2024-11-30 14:14:08 -0400

    i had the app wrapped in thirdweb provider on the app component, i figure the wrapper should go in the index folder as is the root element

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { useDrag } from "react-dnd";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a QueryClient instance  
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider>
+      <App />
+      </ThirdwebProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
